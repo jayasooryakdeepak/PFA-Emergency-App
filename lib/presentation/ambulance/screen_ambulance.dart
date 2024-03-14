ce394000d974c32fd7e070346f17166f55ca6112
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2024-03-14 11:51:32 +0530

    feat:- Carousel image slider added and screen ambulance made scrollaable

--- lib/presentation/ambulance/screen_ambulance.dart
+++ lib/presentation/ambulance/screen_ambulance.dart
@@ -1,8 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
+import 'package:pfa_app/presentation/widgets/phone_card_Widget.dart';
 import 'package:pfa_app/presentation/widgets/service_card_widget.dart';
-
-import '../widgets/phone_card_Widget.dart';
 
 class ScreenAmbulance extends StatelessWidget {
   const ScreenAmbulance({super.key});
@@ -19,50 +18,71 @@ class ScreenAmbulance extends StatelessWidget {
             ),
           ),
         ),
-        body: Column(
-          children: [
-            const PhoneCardWidget(
-              phoneNumber: '9110',
-              text1: 'Call for an Ambulance',
-              text2: '24/7 emergency care',
-            ),
-            // SizedBox(
-            //   height: 10,
-            // ),
-            // const PhoneCardWidget(
-            //   phoneNumber: '9110',
-            //   text1: 'Call for an Ambulance',
-            //   text2: '24/7 emergency care',
-            // ),
-            GridView.count(
-              shrinkWrap: true,
-              crossAxisCount: 2,
-              children: const [
-                ServiceCardWidget(
-                  icon: Icons.local_hospital,
-                  heading: 'Emergency Care',
-                  subtext: '24/7 emergency care for all your medical needs.',
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 200.0,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                  pauseAutoPlayOnTouch: true,
+                  viewportFraction: 1.0,
                 ),
-                ServiceCardWidget(
-                  icon: Icons.local_pharmacy,
-                  heading: 'Pharmacy',
-                  subtext: 'A wide range of medications and over-the-counter.',
-                ),
-                ServiceCardWidget(
-                  icon: Icons.local_dining,
-                  heading: 'Cafeteria',
-                  subtext:
-                      'A variety of food and beverage options to choose from.',
-                ),
-                ServiceCardWidget(
-                  icon: Icons.local_laundry_service,
-                  heading: 'Laundry',
-                  subtext:
-                      'Convenient laundry services for your clothes and linens.',
-                ),
-              ],
-            ),
-          ],
+                items: [
+                  Image.asset('lib/presentation/home/assets/icon/car-crash.gif'),
+                  Image.asset('lib/presentation/home/assets/icon/car-crash.gif'),
+                  Image.asset('lib/presentation/home/assets/icon/car-crash.gif'),
+                ],
+              ),
+              const PhoneCardWidget(
+                phoneNumber: '9110',
+                text1: 'Call for an Ambulance',
+                text2: '24/7 emergency care',
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                children: const [
+                  ServiceCardWidget(
+                    icon: Icons.local_hospital,
+                    heading: 'Emergency Care',
+                    subtext: '24/7 emergency care for all your medical needs.',
+                  ),
+                  ServiceCardWidget(
+                    icon: Icons.local_pharmacy,
+                    heading: 'Pharmacy',
+                    subtext: 'A wide range of medications and over-the-counter.',
+                  ),
+                  ServiceCardWidget(
+                    icon: Icons.local_dining,
+                    heading: 'Cafeteria',
+                    subtext:
+                        'A variety of food and beverage options to choose from.',
+                  ),
+                  ServiceCardWidget(
+                    icon: Icons.local_laundry_service,
+                    heading: 'Laundry',
+                    subtext:
+                        'Convenient laundry services for your clothes and linens.',
+                  ),
+                  ServiceCardWidget(
+                    icon: Icons.local_dining,
+                    heading: 'Cafeteria',
+                    subtext:
+                        'A variety of food and beverage options to choose from.',
+                  ),
+                  ServiceCardWidget(
+                    icon: Icons.local_laundry_service,
+                    heading: 'Laundry',
+                    subtext:
+                        'Convenient laundry services for your clothes and linens.',
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
