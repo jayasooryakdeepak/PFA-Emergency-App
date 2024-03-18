0a71cdf25bdda33b8600df685798959a64e28fde
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2024-03-18 11:30:46 +0530

    Destintion added to map but not able to add route

--- lib/presentation/map/screen_map.dart
+++ lib/presentation/map/screen_map.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pfa_app/core/constants.dart';
 
 class ScreenMaps extends StatefulWidget {
   const ScreenMaps({super.key});
@@ -9,29 +11,66 @@ class ScreenMaps extends StatefulWidget {
 }
 
 class _ScreenMapsState extends State<ScreenMaps> {
+  static const LatLng sourceLocation = LatLng(9.957677, 76.343463);
+  static const LatLng Destination = LatLng(9.965380, 76.340321);
 
-  static const LatLng sourceLocation = LatLng(9.9707186, 76.3420201);
-  static const LatLng Destination = LatLng(9.9707186, 76.3420201);
+  List<LatLng> polylineCoordinates = [];
 
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      mapAPIKey,
+      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+      PointLatLng(Destination.latitude, Destination.longitude),
+    );
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+        (PointLatLng point) => polylineCoordinates.add(
+          LatLng(
+            point.latitude,
+            point.longitude,
+          ),
+        ),
+      );
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    super.initState();
+  }
 
   static const _initialCameraPosition = CameraPosition(
     target: sourceLocation,
-    zoom: 11.5,
+    zoom: 14.5,
   );
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: _initialCameraPosition,
+        polylines: {
+          Polyline(
+            polylineId: PolylineId("route"),
+            points: polylineCoordinates,
+            color: Colors.black,
+          ),
+        },
         markers: {
-          Marker(
-          markerId: MarkerId("Source"),
-          position: sourceLocation,
-        ),
-        }
+          const Marker(
+            markerId: MarkerId("Source"),
+            position: sourceLocation,
+          ),
+          const Marker(
+            markerId: MarkerId("Destination"),
+            position: Destination,
+          ),
+        },
       ),
     );
   }
